3d786cb38ce149d64c55c16cc51f5d014d86b241
Qizzzer <97835700+Qizzzer@users.noreply.github.com>
2024-04-20 11:48:27 +0800

    Update UvVis.py
    功能改进：实时显示和坐标
    待完成：波长校准和输出谱图

--- UvVis.py
+++ UvVis.py
@@ -2,11 +2,28 @@ import os
 import time
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QHBoxLayout,QComboBox, QGroupBox, QGridLayout, QScrollArea
-from PyQt5.QtGui import QPixmap,QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QImage ,QCursor
+from PyQt5.QtCore import Qt, QTimer
 from pypylon import pylon
 import cv2
 import sys
+
+class ImageLabel(QLabel):
+    def __init__(self, parent=None):
+        super(ImageLabel, self).__init__(parent)
+        self.setMouseTracking(True)
+
+    def mouseMoveEvent(self, event):
+        # Get the coordinates of the mouse in the label
+        x, y = event.x(), event.y()
+        # Calculate the real coordinates in the image if the image is scaled
+        pixmap = self.pixmap()
+        if pixmap:
+            scale_w = pixmap.width() / self.width()
+            scale_h = pixmap.height() / self.height()
+            real_x, real_y = int(x * scale_w), int(y * scale_h)
+            self.setToolTip(f"Coordinates: ({real_y}, {real_x})")
+            self.setCursor(QCursor(Qt.CrossCursor))
 
 class MockCamera:
     def __init__(self):
@@ -45,6 +62,8 @@ class CameraControlApp(QWidget):
     def __init__(self):
         super().__init__()
         self.camera = None
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateImageDisplay)
         self.initUI()
 
     def initUI(self):
@@ -70,7 +89,7 @@ class CameraControlApp(QWidget):
         # Camera Controls Group
         cameraControlGroup = QGroupBox("Camera Controls")
         cameraControlLayout = QVBoxLayout()
-        self.initCameraBtn = QPushButton("Open Camera")
+        self.initCameraBtn = QPushButton("Init Camera")
         self.captureAqueousBtn = QPushButton("Capture Water")
         self.captureSampleBtn = QPushButton("Capture Sample")
         self.closeCameraBtn = QPushButton("Close Camera")
@@ -90,13 +109,41 @@ class CameraControlApp(QWidget):
         self.averageSamplesBtn = QPushButton("Average Sample")
         self.averageWatersBtn = QPushButton("Average Water")
         self.findMaxSumAreasBtn = QPushButton("Find Max Sum Areas")
+        self.processImagesBtn = QPushButton("Process Image Areas")
         imageProcessingLayout.addWidget(self.averageSamplesBtn)
         imageProcessingLayout.addWidget(self.averageWatersBtn)
         imageProcessingLayout.addWidget(self.findMaxSumAreasBtn)
+        imageProcessingLayout.addWidget(self.processImagesBtn)
         imageProcessingGroup.setLayout(imageProcessingLayout)
 
+        # Spectral wavelength calibration
+        spectralGroup = QGroupBox("Spectral wavelength calibration")
+        spectralLayout = QGridLayout()
+
+        # Exposure Time Input 需要改写变量名
+        spectralLayout.addWidget(QLabel("437nm :"), 0, 0)
+        self.spectral437nm = QLineEdit("200")  #
+        self.spectral437nm.setFixedWidth(50)
+        spectralLayout.addWidget(self.spectral437nm, 0, 1)
+
+        spectralLayout.addWidget(QLabel("546nm:"), 0, 2)
+        self.spectral546nm = QLineEdit("300")  #
+        self.spectral546nm.setFixedWidth(50)
+        spectralLayout.addWidget(self.spectral546nm, 0, 3)
+
+        spectralLayout.addWidget(QLabel("577nm:"), 1, 0)
+        self.spectral577nm = QLineEdit("400")  #
+        self.spectral577nm.setFixedWidth(50)
+        spectralLayout.addWidget(self.spectral577nm, 1, 1)
+
+        spectralLayout.addWidget(QLabel("579nm:"), 1, 2)
+        self.spectral579nm = QLineEdit("400",)  #
+        self.spectral579nm.setFixedWidth(50)
+        spectralLayout.addWidget(self.spectral579nm, 1, 3)
+        spectralGroup.setLayout(spectralLayout)
+
         # Image Display Area with Scroll
-        self.imageDisplayLabel = QLabel()
+        self.imageDisplayLabel = ImageLabel(self)
         self.imageDisplayLabel.setScaledContents(True)
         scrollArea = QScrollArea()
         scrollArea.setWidgetResizable(True)
@@ -107,7 +154,7 @@ class CameraControlApp(QWidget):
         topLayout.addWidget(cameraSettingsGroup)
         topLayout.addWidget(cameraControlGroup)
         topLayout.addWidget(imageProcessingGroup)
-
+        topLayout.addWidget(spectralGroup)
         mainLayout.addLayout(topLayout)
         mainLayout.addWidget(self.directoryDisplayLabel)
         mainLayout.addWidget(scrollArea)
@@ -122,6 +169,7 @@ class CameraControlApp(QWidget):
         self.averageSamplesBtn.clicked.connect(lambda: self.averageImages("sample_picture"))
         self.averageWatersBtn.clicked.connect(lambda: self.averageImages("water_picture"))
         self.findMaxSumAreasBtn.clicked.connect(self.findMaxSumAreas)
+        self.processImagesBtn.clicked.connect(self.process_image_areas)
 
     def listCameras(self):
         devices = pylon.TlFactory.GetInstance().EnumerateDevices()
@@ -142,11 +190,18 @@ class CameraControlApp(QWidget):
                     self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateDevice(device))
                     break
         self.camera.Open()
+        if self.camera.IsOpen():
+            self.timer.start(100)  # Start updating the display every 100 ms
         exposureTime = int(self.exposureTimeInput.text())
         if selectedCamera != "Virtual Camera":
             self.camera.ExposureTimeAbs.SetValue(exposureTime)
         print(f"Camera initialized with exposure time: {exposureTime}us")
         self.updateDirectoryDisplay(f"Camera initialized with exposure time: {exposureTime}us")
+
+    def openCamera(self, imageType):
+        self.camera.Open()
+        if self.camera.IsOpen():
+            self.timer.start(100)  # Start updating the display every 100 ms
 
     def captureImage(self, imageType):
         if not self.camera or not self.camera.IsOpen:
@@ -158,43 +213,58 @@ class CameraControlApp(QWidget):
         save_directory = imageType
         if not os.path.exists(save_directory):
             os.makedirs(save_directory)
-
+        self.timer.stop()
         # Capture and save the image
         self.camera.StartGrabbingMax(1)
-        grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        grabResult = self.camera.RetrieveResult(5000,None)
         if grabResult.GrabSucceeded():
             img = grabResult.Array
             timestamp = time.strftime("%Y%m%d_%H%M%S")
-            filename = f"{timestamp}.png"
+            filename = f"{imageType}_{timestamp}.png"
             cv2.imwrite(os.path.join(save_directory, filename), img)
             imagePath = os.path.join(save_directory, filename)
-            self.updateImageDisplay(imagePath)
+            image = QImage(grabResult.Array.data, grabResult.Array.shape[1], grabResult.Array.shape[0], QImage.Format_Grayscale8)
+            pixmap = QPixmap.fromImage(image)
+            self.imageDisplayLabel.setPixmap(pixmap)
             self.directoryDisplayLabel.setText(f"Last saved in: {os.path.abspath(save_directory)}")
             print(f"Image saved in {imagePath}")
-            self.updateDirectoryDisplay(f"Image saved in {imagePath}")
+            self.updateDirectoryDisplay(f"Image saved in {imagePath}，image maxpixe is {img.max()}")
         grabResult.Release()
+
+    def updateImageDisplay(self):
+        if self.camera.IsOpen():
+            result = self.camera.RetrieveResult(5000, None)
+            if result.GrabSucceeded():
+                image = QImage(result.Array.data, result.Array.shape[1], result.Array.shape[0], QImage.Format_Grayscale8)
+                #image = QImage(result.Array.data, result.Array.shape[1], result.Array.shape[0], QImage.Format_RGB888)
+                pixmap = QPixmap.fromImage(image)
+                self.imageDisplayLabel.setPixmap(pixmap.scaled(self.imageDisplayLabel.size(), Qt.KeepAspectRatio))
+    def updateDirectoryDisplay(self, message):
+        self.directoryDisplayLabel.setText(message)
+
+    def closeCamera(self):
+        if self.camera and self.camera.IsOpen:
+            self.camera.Close()
+            self.updateDirectoryDisplay(f"Camera closed")
+            print("Camera closed")
+
     def averageImages(self, imageType):
         # Example to average images in the 'sample_picture' folder
         folder_path = imageType
         average_image = self.average_images_in_folder(folder_path)
         if average_image is not None:
-            # Convert to QPixmap for display
-            # Convert the averaged image from float64 to uint8 before color conversion
-            average_image_uint8 = cv2.normalize(average_image, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-
-            average_image = cv2.cvtColor(average_image_uint8, cv2.COLOR_BGR2RGB)
-            height, width, channel = average_image.shape
-            bytesPerLine = 3 * width
-            qImg = QImage(average_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            height, width = average_image.shape
+            bytesPerLine = width
+            qImg = QImage(average_image.data, width, height, bytesPerLine, QImage.Format_Grayscale8)
             pixmap = QPixmap.fromImage(qImg)
             self.imageDisplayLabel.setPixmap(pixmap)
             # Save the averaged image
-            cv2.imwrite(os.path.join(folder_path, 'average_image.png'), average_image)
+            cv2.imwrite(os.path.join(folder_path, f'average_{folder_path}.png'), average_image)
             print("Average image saved")
-            self.updateDirectoryDisplay(f"Average image saved in {folder_path}")
+            self.updateDirectoryDisplay(f"Average image saved in {folder_path}/average_{folder_path}.png")
 
     @staticmethod
-    def average_images_in_folder(folder_path,):
+    def average_images_in_folder(folder_path):
         # Check if the folder exists
         if not os.path.exists(folder_path):
             print(f"Folder '{folder_path}' does not exist.")
@@ -211,7 +281,7 @@ class CameraControlApp(QWidget):
             # Check if the file is an image (you can add more image extensions if needed)
             if file_path.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.gif')):
                 # Read the image
-                image = cv2.imread(file_path)
+                image = cv2.imread(file_path,cv2.IMREAD_GRAYSCALE)
 
                 # Check if the image was read successfully
                 if image is not None:
@@ -227,25 +297,27 @@ class CameraControlApp(QWidget):
         if image_count > 0:
             # Calculate the average by dividing the sum by the count
             average_image = (sum_of_images / image_count).astype(np.float64)
-            return average_image
+            return average_image.astype(np.uint8)
         else:
             print(f"No valid images found in '{folder_path}'.")
             return None
 
     def findMaxSumAreas(self):
         # Example usage with an averaged image
-        imagePath = 'sample_picture/average_image.png'
-        average_image = cv2.imread(imagePath)
+        imagePath = 'sample_picture/average_sample_picture.png'
+        average_image = cv2.imread(imagePath,cv2.IMREAD_GRAYSCALE)
         if average_image is not None:
             max_sums, starting_rows = self.find_non_overlapping_max_sum_areas(average_image)
+            message = ""
             for i, start_row in enumerate(starting_rows, start=1):
                 end_row = start_row + 50  # Assuming 50 as rows_per_area
+                message = message + "   " + f"Area {i}: Rows {start_row} to {end_row}" + "  "
+                self.directoryDisplayLabel.setText(message)
                 print(f"Area {i}: Rows {start_row} to {end_row}")
                 # Update resultDisplayLabel or draw on the image as needed
 
     @staticmethod
     def find_non_overlapping_max_sum_areas(average_image, num_areas=3, rows_per_area=50, min_gap=200):
-        average_image = average_image[:, :, 0]
         # Initialize variables to store the maximum sums and their starting rows
         max_sums = []
         starting_rows = []
@@ -272,24 +344,69 @@ class CameraControlApp(QWidget):
             # Set the rows in the current area and a buffer above and below it to zero
             buffer = min_gap + rows_per_area
             average_image[max(0, start_row - buffer):min(end_row + buffer, average_image.shape[0]), :] = 0
-
         return max_sums, starting_rows
+    def process_image_areas(self):
+        directories = ['water_picture', 'sample_picture']
+        results = []
 
-    def updateImageDisplay(self, imagePath):
-        pixmap = QPixmap(imagePath)
-        self.imageDisplayLabel.setPixmap(
-            pixmap.scaled(self.imageDisplayLabel.width(), self.imageDisplayLabel.height(), Qt.KeepAspectRatio))
+        for dir_name in directories:
+            # Construct path to the average image
+            image_path = os.path.join(dir_name, 'average_image.png')
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Assuming grayscale for simplicity
+            if image is not None:
+                # Assuming find_non_overlapping_max_sum_areas is already defined
+                _, starting_rows = self.find_non_overlapping_max_sum_areas(image)
 
+                # Extract and process the specified areas
+                for start_row in starting_rows[:3]:  # Assuming we need the first three areas
+                    end_row = start_row + 50  # Assuming the area height is 50 rows
+                    area = image[start_row:end_row, :]
 
-    def updateDirectoryDisplay(self, message):
-        self.directoryDisplayLabel.setText(message)
+                    # Example operation: calculate the sum of the area
+                    area_sum = np.sum(area)
+                    results.append(area_sum)
 
+                    # Perform other operations as needed
 
-    def closeCamera(self):
-        if self.camera and self.camera.IsOpen:
-            self.camera.Close()
-            print("Camera closed")
+        # Save the results to a text file
+        with open('image_processing_results.txt', 'w') as file:
+            for result in results:
+                file.write(f"{result}\n")
 
+        print("Image processing complete. Results saved to image_processing_results.txt.")
+
+    @staticmethod
+    def split_image(path, first_area, second_area, third_area, length):
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        t = img[first_area:first_area + length, :]
+        s = img[second_area:second_area + length, :]
+        c = img[third_area:third_area + length, :]
+        return t, s, c
+
+    @staticmethod
+    def average_intense(t, s, c):
+        t_avg = np.average(t, axis=0)
+        s_avg = np.average(s, axis=0)
+        c_avg = np.average(c, axis=0)
+        return t_avg, s_avg, c_avg
+
+    # 自己写的输出光谱函数
+    def image2spectal(self):
+        average_sample_picture = cv2.imread("sample_picture/average_sample_picture",cv2.IMREAD_GRAYSCALE)
+        # get three area
+        max_sums, starting_rows = self.find_non_overlapping_max_sum_areas(average_sample_picture)
+        first, second, third = starting_rows
+        # average area
+        water_toushe, water_sanshe, water_canbi = self.split_image("water_picture/average_water_picture", first, second, third, 50)
+        water_avg_t, water_avg_s, water_avg_c = self.average_intense(water_toushe, water_sanshe, water_canbi)
+        sample_toushe, sample_sanshe, sample_canbi = self.split_image("sample_picture/average_sample_picture", first, second, third, 50)
+        sample_avg_t, sample_avg_s, sample_avg_c = self.average_intense(sample_toushe, sample_sanshe, sample_canbi)
+        # calculate xiaoguang
+        xiaoguang = np.log10(sample_avg_c / sample_avg_t)
+        with open('output/xiaoguangdu.txt', 'w') as f:
+            for i in range(len(xiaoguang)):
+                # 将两个列表中的元素用逗号隔开，然后写入文件中
+                f.write(f"{[i]},{xiaoguang[i]}\n")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
